cdaac13e43ca7561dfe7849be464d66fd33bcc4a
endaaman <ken@endaaman.me>
2023-01-24 16:22:38 +0900

    tailwind

---
+++ src/components/Article.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+
+const query = graphql`
+  query {
+    allStrapiArticle {
+      edges {
+        node {
+          id
+          title
+          digest
+          tags {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Article = () => (
+  <StaticQuery
+    query={query}
+    render={(data) => (
+      <div>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">ボタン</button>
+        <ul>
+          {data.allStrapiArticle.edges.map((b) => (
+            <li key={b.node.strapiId}>
+              <div>
+                <p>{b.node.title}</p>
+                <pre>{JSON.stringify(b.node)}</pre>
+                {/* {b.node.tags.map((t) => ( */}
+                {/*     <p>{t.name}</p> */}
+                {/* ))} */}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )}
+  />
+);
+
+export default Article;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,13 @@
-// import React from 'react';
-// import Title from '@/components/Title';
-
-import { StaticQuery, graphql } from 'gatsby';
-
-const query = graphql`
-  query {
-    allStrapiArticle {
-      edges {
-        node {
-          id
-          title
-          digest
-          tags {
-            name
-          }
-        }
-      }
-    }
-  }
-`;
+import React from 'react';
+import Sidebar from '@/components/Sidebar';
+import Article from '@/components/Article';
+import Header from '@/components/Header';
 
 const IndexPage = () => (
-  <StaticQuery
-    query={query}
-    render={(data) => (
-      <div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">ボタン</button>
-        <ul>
-          {data.allStrapiArticle.edges.map((b) => (
-            <li key={b.node.strapiId}>
-              <div>
-                <p>{b.node.title}</p>
-                <pre>{JSON.stringify(b.node)}</pre>
-                {/* {b.node.tags.map((t) => ( */}
-                {/*     <p>{t.name}</p> */}
-                {/* ))} */}
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    )}
-  />
+  <div>
+    <Sidebar/>
+    <Article/>
+  </div>
 );
 
 export default IndexPage;
